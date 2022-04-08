9f40f8719afea73211433260a7706f55945f6411
An <10003601+a-happin@users.noreply.github.com>
2022-04-08 19:29:23 +0900

    Add get-minecraft-vanilla-data.ts

---
+++ get-minecraft-vanilla-data.ts
@@ -0,0 +1,127 @@
+type VersionManifest = {
+  latest: {
+    release: string
+    snapshot: string
+  }
+  versions: readonly {
+    id: string
+    type: string
+    url: string
+    time: string
+    releaseTime: string
+  }[]
+}
+
+const exists = async (filePath: string): Promise <boolean> => {
+  try
+  {
+    await Deno.lstat (filePath)
+    return true
+  }
+  catch (error)
+  {
+    if (error instanceof Deno.errors.NotFound)
+    {
+      return false
+    }
+    else
+    {
+      throw error
+    }
+  }
+}
+
+const fetchJSON = (url: string) => fetch (url).then ((response) => response.json ())
+
+const download = async (url: string, filePath: string): Promise <void> => {
+  const data = await fetch (url)
+    .then ((response) => response.arrayBuffer ())
+    .then ((arrayBuffer) => new Uint8Array (arrayBuffer))
+  await Deno.writeFile (filePath, data)
+}
+
+if (Deno.args.length === 0)
+{
+  console.log (`Download Minecraft vanilla datapacks and resourcepacks
+
+Usage:
+ deno run --allow-net --allow-read --allow-run --allow-write get-minecraft-vanilla-data <versions>
+
+versions:
+  release   -- latest release
+  snapshot  -- latest snapshot
+  <version> -- specified version
+`)
+}
+else
+{
+  const version_manifest = await fetchJSON ('https://launchermeta.mojang.com/mc/game/version_manifest.json') as VersionManifest
+
+  const targets = new Set ()
+  for (const arg of Deno.args)
+  {
+    if (arg === 'release')
+    {
+      targets.add (version_manifest.latest.release)
+    }
+    else if (arg === 'snapshot')
+    {
+      targets.add (version_manifest.latest.snapshot)
+    }
+    else
+    {
+      targets.add (arg)
+    }
+  }
+
+  const tasks = []
+  for (const target of targets)
+  {
+    const target_version = version_manifest.versions.find ((x) => x.id === target)
+    if (target_version !== undefined)
+    {
+      const dirPath = `./${target}`
+      if (await exists (dirPath))
+      {
+        console.error (`${dirPath} already exists`)
+      }
+      else
+      {
+        tasks.push (async () => {
+          await Deno.mkdir (dirPath)
+          const version = await fetchJSON (target_version.url)
+          const clientPath = `${dirPath}/client.jar`
+
+          console.log (`Downloading ${clientPath} ...`)
+          await download (version.downloads.client.url, clientPath)
+          console.log (`Downloaded ${clientPath}`)
+
+          console.log (`Extracting ${clientPath} ...`)
+          await Deno.run ({
+            cmd: ['unzip', '-q', 'client.jar', 'assets/*', 'data/*', '-x', '*/.mcassetsroot'],
+            cwd: dirPath})
+          .status ()
+          console.log (`Extracted ${clientPath}`)
+
+          const lang = 'ja_jp'
+          const langFilePath = `${dirPath}/assets/minecraft/lang/${lang}.json`
+          console.log (`Downloading ${langFilePath} ...`)
+          const assetIndex = await fetchJSON (version.assetIndex.url)
+          const hash = assetIndex.objects[`minecraft/lang/${lang}.json`].hash
+          const lang_json = await fetchJSON (`http://resources.download.minecraft.net/${hash.slice (0, 2)}/${hash}`)
+          await Deno.writeTextFile (langFilePath, JSON.stringify (lang_json, undefined, 2))
+          console.log (`Downloaded ${langFilePath}`)
+
+          await Deno.remove (clientPath)
+          console.log (`Removed ${clientPath}`)
+        })
+      }
+    }
+    else
+    {
+      console.error (`${target} is not a valid version`)
+    }
+  }
+
+  Promise.all (tasks.map ((task) => task ()))
+}
