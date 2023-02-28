1f2e240b12701b98c6d1f614a67e99af7778a7ce
a_happin <10003601+a@users.noreply.github.com>
2023-02-28 22:51:04 +0900

    Fix get-minecraft-vanilla-data

--- get-minecraft-vanilla-data.ts
+++ get-minecraft-vanilla-data.ts
@@ -50,30 +50,64 @@ else
         tasks.push (async () => {
           await Deno.mkdir (dirPath)
           const version = await fetchJSON (target_version.url)
-          const clientPath = `${dirPath}/client.jar`
 
-          console.log (`Downloading ${clientPath} ...`)
-          await download (version.downloads.client.url, clientPath)
-          console.log (`Downloaded ${clientPath}`)
+          const internal_tasks = []
+          internal_tasks.push (async () => {
+            const clientPath = `${dirPath}/client.jar`
 
-          console.log (`Extracting ${clientPath} ...`)
-          await Deno.run ({
-            cmd: ['unzip', '-q', 'client.jar', 'assets/*', 'data/*', '-x', '*/.mcassetsroot'],
-            cwd: dirPath})
-          .status ()
-          console.log (`Extracted ${clientPath}`)
+            console.log (`Downloading ${version.downloads.client.url} to ${clientPath} ...`)
+            await download (version.downloads.client.url, clientPath)
+            console.log (`Downloaded ${clientPath}`)
 
-          const lang = 'ja_jp'
-          const langFilePath = `${dirPath}/assets/minecraft/lang/${lang}.json`
-          console.log (`Downloading ${langFilePath} ...`)
-          const assetIndex = await fetchJSON (version.assetIndex.url)
-          const hash = assetIndex.objects[`minecraft/lang/${lang}.json`].hash
-          const lang_json = await fetchJSON (`https://resources.download.minecraft.net/${hash.slice (0, 2)}/${hash}`)
-          await Deno.writeTextFile (langFilePath, JSON.stringify (lang_json, undefined, 2))
-          console.log (`Downloaded ${langFilePath}`)
+            console.log (`Extracting ${clientPath} ...`)
+            await Deno.run ({
+              cmd: ['unzip', '-q', 'client.jar', 'assets/*', 'data/*', '-x', '*/.mcassetsroot'],
+              cwd: dirPath
+            }).status ()
+            console.log (`Extracted ${clientPath}`)
 
-          await Deno.remove (clientPath)
-          console.log (`Removed ${clientPath}`)
+            const lang = 'ja_jp'
+            const langFilePath = `${dirPath}/assets/minecraft/lang/${lang}.json`
+            console.log (`Fetching AssetIndex from ${version.assetIndex.url} ...`)
+            const assetIndex = await fetchJSON (version.assetIndex.url)
+            console.log (`Fetched AssetIndex`)
+            const hash = assetIndex.objects[`minecraft/lang/${lang}.json`].hash
+            const langFileURL = `https://resources.download.minecraft.net/${hash.slice (0, 2)}/${hash}`
+            console.log (`Downloading ${langFileURL} to ${langFilePath} ...`)
+            const lang_json = await fetchJSON (langFileURL)
+            await Deno.writeTextFile (langFilePath, JSON.stringify (lang_json, undefined, 2))
+            console.log (`Downloaded ${langFilePath}`)
+
+            console.log (`Removing ${clientPath} ...`)
+            await Deno.remove (clientPath)
+            console.log (`Removed ${clientPath}`)
+          })
+
+          internal_tasks.push (async () => {
+            const serverPath = `${dirPath}/server.jar`
+
+            console.log (`Downloading ${version.downloads.server.url} to ${serverPath} ...`)
+            await download (version.downloads.server.url, serverPath)
+            console.log (`Downloaded ${serverPath}`)
+
+            console.log (`Generating Registory Data...`)
+            await Deno.run ({
+              cmd: ['java.exe', '-DbundlerMainClass=net.minecraft.data.Main', '-jar', 'server.jar', '--reports'],
+              cwd: dirPath
+            }).status ()
+            console.log (`Generated Registory Data`)
+
+            console.log (`Removing ${serverPath} ...`)
+            await Promise.all ([
+              Deno.remove (serverPath),
+              Deno.remove (`${dirPath}/libraries`, {recursive: true}),
+              Deno.remove (`${dirPath}/logs`, {recursive: true}),
+              Deno.remove (`${dirPath}/versions`, {recursive: true}),
+            ])
+            console.log (`Removed ${serverPath}`)
+          })
+
+          await Promise.all (internal_tasks.map ((task) => task ()))
         })
       }
     }
