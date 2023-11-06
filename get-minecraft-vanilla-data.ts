4355f59b6974618bbf407ead292b94558e47102a
a_happin <10003601+a@users.noreply.github.com>
2023-11-06 15:44:43 +0900

    Add to registries.ts

--- get-minecraft-vanilla-data.ts
+++ get-minecraft-vanilla-data.ts
@@ -193,6 +193,30 @@ for (const target_version of get_target_versions ())
         await writeJSONFile (`${dirPath}/processed/reports/registries.json`, processed_registries)
       })
     }
+
+    // registries.ts
+    await Deno.writeTextFile (`${dirPath}/registries.ts`, (() => {
+      return [
+        "import registries from './generated/reports/registries.json' assert { type: 'json' }",
+        "",
+        "type OmitMinecraft <T> = T extends 'minecraft:' ? ':' : T extends `minecraft:${infer U}` ? U : T",
+        "const omit_minecraft = (location: string) => location === 'minecraft:' ? ':' : location.startsWith ('minecraft:') ? location.slice (10) : location",
+        "",
+        "type ProcessedRegistries = {",
+        "  [k in OmitMinecraft <keyof typeof registries>]: OmitMinecraft <keyof typeof registries[`minecraft:${k}`]['entries']>[]",
+        "}",
+        "",
+        "const processed: {[k in string]: string[]} = {}",
+        "for (const [key, value] of Object.entries (registries))",
+        "{",
+        "  processed[omit_minecraft (key)] = Object.keys (value.entries).map (omit_minecraft).sort ()",
+        "}",
+        "",
+        "export default processed as ProcessedRegistries",
+        "",
+      ].join ('\n')
+    }) ())
+
   })
 }
 
